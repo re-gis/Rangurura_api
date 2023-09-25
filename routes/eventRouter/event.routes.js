7b63235208ea0690a0d72bb3be2cae7a5fd982be
Edouard <siboedouard88@gmail.com>
2023-09-25 23:39:53 +0200

    codes with displaying events

--- routes/eventRouter/event.routes.js
+++ routes/eventRouter/event.routes.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const eventRouter = express.Router();
 const eventController=require('../../controllers/events/event.controller'); 
+const displayAllEvents=require('../../controllers/events/displayEvents.controller').displayAllEvents;
+const displayLeaderEvents=require('../../controllers/events/displayEvents.controller').displayLeadersEvents;
 
-eventRouter.post('/sendevents',eventController); //this is the logic to send the event
+eventRouter.post('/sendevents',eventController); //this is the route to send the event
+
+eventRouter.get('/allevents',displayAllEvents); //this is the route to get the event
+eventRouter.post('/leaderEvent',displayAllEvents); //this is the route to get the event of the given leader
 module.exports = eventRouter;
