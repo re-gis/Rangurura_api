66141df53d650ba54741f6aa9929b76d77d8d0c5
D Regis <irumvaregisdmc@gmail.com>
2023-09-26 08:48:35 +0200

    Format

--- controllers/events/event.controller.js
+++ controllers/events/event.controller.js
@@ -1,63 +1,69 @@
 //this is the logic about the events
-const EventSchema=require('../../models/event.model');
+const EventSchema = require("../../models/event.model");
 
-const eventController=async(req,res)=>{
-    if (!req.user) {
-        return res.status(401).json({ message: "Unauthorized" });
-      }
+const eventController = async (req, res) => {
+  if (!req.user) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 
-const indangamuntu = req.user.indangamuntu;
-//this is to get the input from the user
-const {eventName,
+  const indangamuntu = req.user.indangamuntu;
+  //this is to get the input from the user
+  const {
+    eventName,
     organizationLevel,
     location,
-     category,
+    category,
     startDate,
-     endDate,
-     endTime,
-     startTime,
-     description}=req.body;   
+    endDate,
+    endTime,
+    startTime,
+    description,
+  } = req.body;
 
-    if( !eventName ||
-        !organizationLevel||
-        !location||
-        !category ||
-        !startDate ||  
-        !endDate ||        
-        !endTime ||     
-        !startTime ||
-        !description){
-        return    res.status(400).json({message:"Uzuza ibisabwa byose maze wohereze!"});
-       }
-      
-    try{
-        const newEvent=await new EventSchema({
-            eventName,
-            organizationLevel,
-            location,
-            category,
-            startDate,
-             endDate,
-             endTime,
-             startTime,
-             description,
-             indangamuntu //this is for logged
-        })
-        
-       
+  if (
+    !eventName ||
+    !organizationLevel ||
+    !location ||
+    !category ||
+    !startDate ||
+    !endDate ||
+    !endTime ||
+    !startTime ||
+    !description
+  ) {
+    return res
+      .status(400)
+      .json({ message: "Uzuza ibisabwa byose maze wohereze!" });
+  }
 
-//this is to save the problem to the db
-await newEvent.save()
-return res.status(200).json({msg:"Wamaze gutanga event yawe tegereza abo igenewe mu mwanya muto!"});
-    }catch(error){
-        res.status(500).json({msg:"something went wrong please try again later"});
-        console.log(error);
-    }
-}
+  try {
+    const newEvent = await new EventSchema({
+      eventName,
+      organizationLevel,
+      location,
+      category,
+      startDate,
+      endDate,
+      endTime,
+      startTime,
+      description,
+      indangamuntu, //this is for logged
+    });
+
+    //this is to save the problem to the db
+    await newEvent.save();
+    return res
+      .status(200)
+      .json({
+        msg: "Wamaze gutanga event yawe tegereza abo igenewe mu mwanya muto!",
+      });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ msg: "something went wrong please try again later" });
+    console.log(error);
+  }
+};
 
 //this is to export the eventcontroller
-module.exports=eventController;
-
-
-
-
+module.exports = eventController;
