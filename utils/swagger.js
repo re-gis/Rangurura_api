260c69750082ed09049a662da340224251980f98
D Regis <irumvaregisdmc@gmail.com>
2023-10-04 08:27:20 +0200

    Swagger

--- utils/swagger.js
+++ utils/swagger.js
@@ -1,22 +1,17 @@
-const swaggerJSDoc = require('swagger-jsdoc');
-const user=require("../routes/leadersRoutes/leaders.routes")
-const options={
-    definition:{
-        openapi:"3.0.0",
-    info:{
-title:"Rangurura",
-version:"1.0.0",
-description:"This is the swagger documentation of rangurura"
+const swaggerJSDoc = require("swagger-jsdoc");
+const user = require("../routes/leadersRoutes/leaders.routes");
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Rangurura",
+      version: "1.0.0",
+      description: "This is the swagger documentation of rangurura",
     },
-},
-apis:["../routes/userRouters/user.routes.js"]
-
-
-}
+  },
+  apis: ["../routes/userRouters/user.routes.js"],
+};
 
 const swaggerSpec = swaggerJSDoc(options);
 
 module.exports = swaggerSpec;
-
-
-
